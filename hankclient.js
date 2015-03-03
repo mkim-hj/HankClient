b3280c46a9b63198844e601c3ef2e138f033548f
mkim-hj <maruchi.kim@gmail.com>
2015-03-02 21:47:36 -0800

    working get request

--- hankclient.js
+++ hankclient.js
@@ -1,6 +1,52 @@
 if (Meteor.isClient) {
   // counter starts at 0
 
+  Template.body.helpers({
+    getDigits: function() {
+      return Session.get("digits");
+    },
+    getHits: function() {
+      return Session.get("hits");
+    },
+    getMisses: function() {
+      return Session.get("misses");
+    },
+    getIslands: function() {
+      return Session.get("islands");
+    }
+  });
+
+  Template.body.events({
+    'submit #cal-card-info': function(event) {
+      event.preventDefault();
+      var digits = $('input.digits').val();
+      console.log(digits);
+
+      var CardObject = Parse.Object.extend("CardObject");
+      var query = new Parse.Query(CardObject);
+      query.equalTo("digits", digits);
+
+      query.find({
+        success: function(results) {
+          console.log("Successfully retrieved " + results.length + " scores.");
+          // Do something with the returned Parse.Object values
+          if (results.length == 1) {
+            var user = results[0];
+
+            Session.set("digits", user.attributes.digits);
+            Session.set("hits", user.attributes.hits);
+            Session.set("misses", user.attributes.misses);
+            Session.set("islands", user.attributes.islands);
+          } else {
+            alert("More than 2 results");
+          }
+        },
+        error: function(error) {
+          alert("Error: " + error.code + " " + error.message);
+        }
+      });
+    }
+  })
 }
 
 if (Meteor.isServer) {
